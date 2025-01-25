61b476354cb3ba987989dc4952d2fc6e00d1c8f6
aideed <83303507+aideedh@users.noreply.github.com>
2025-01-25 18:25:14 -0500

    v2 start

---
+++ Assignment1/MyGameV2.py
@@ -0,0 +1,19 @@
+"""
+1. Use of Random and Math Module:
+Write a  program in python to simulate a game called MyGame.py. 
+In this game you will use three variables called player1, player2 and player3. 
+Each player rolls a dice, which means that the variables are initialized with random 
+integers ranging from 1 to 6. Compare the values of player1, player2 and player3 to find 
+the highest value. Print that highest value. Use comments and import statements wherever 
+applicable. Give screenshots of the code, output and git repository containing this program.
+"""
+
+import random
+
+player1 = random.randint(1,6)
+player2 = random.randint(1,6)
+player3 = random.randint(1,6)
+
+print (f"Player 1 rolled: {player1}")
+print (f"Player 2 rolled: {player2}")
+print (f"Player 3 rolled: {player3}")
