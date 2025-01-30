60f204e5ad62e1d1415c2111eb2340cc1e12d982
aideed <83303507+aideedh@users.noreply.github.com>
2025-01-29 21:34:22 -0500

    added while

--- Assignment1/greet.py
+++ Assignment1/greet.py
@@ -8,13 +8,20 @@ Explain your logic. Create a version2 of this problem in your git repository.
  Give screenshots of the code, outputs and the git repository
 '''
 
-#v1
+# #v1
 
+# name = input("What is your name: ")
+# print (f"Good Morning {name}") # or print("Good Morning" + str(name))
+
+# #v2
+
+# name = input("What is your name: ")
+# for loop in range(1,4):
+#     print("Good Morning" + str(name))
+
+#or 
+
+count = 0
 name = input("What is your name: ")
-print (f"Good Morning {name}") # or print("Good Morning" + str(name))
-
-#v2
-
-
-for loop in range(1,4):
-    print("Good Morning" + str(name))
+while count <= 3:
+    print ("Good morning", (name))
