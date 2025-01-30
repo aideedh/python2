97810a2c7cc94a79d182f271db5f92e0e511cc1f
aideed <83303507+aideedh@users.noreply.github.com>
2025-01-29 21:15:45 -0500

    finish logic

--- Assignment1/MyGameV2.py
+++ Assignment1/MyGameV2.py
@@ -33,3 +33,25 @@ for rolls in range (1,3):
     print(f"Player1 Roll #{rolls} - Score = {rollPlayer1}")
     print(f"Player2 Roll #{rolls} - Score = {rollPlayer2}")
     print(f"Player3 Roll #{rolls} - Score = {rollPlayer3}")
+
+# SCORE LOGIC - BELLOW
+
+# highest score logic - set player1 as higest initially
+higestScore = scorePlayer1
+
+# check if player 2 has the higeest score and print
+if (scorePlayer2 > scorePlayer1 and scorePlayer2 > scorePlayer3):
+    higestScore = scorePlayer2
+    print("---------------------------")
+    print (f"Player2 had the highest score of: {higestScore}")
+
+# check if player 3 has the higeest score and print
+elif (scorePlayer3 > scorePlayer1 and scorePlayer3 > scorePlayer2):
+    higestScore = scorePlayer3
+    print("---------------------------")
+    print (f"Player3 had the highest score of: {higestScore}")
+    
+# If nither player 2 or player 3 have the highest player 1 wins
+else:
+    print("---------------------------")
+    print (f"Player1 had the highest score of: {higestScore}")
---
+++ Assignment1/MyGameV3.py
@@ -0,0 +1,10 @@
+'''
+Implement the following as version 3 and add the code to computer average score of all three players in problem 2.  
+The average will be calculated and printed twice. Once using ‘/’ operator and then using ‘//’ operator.  
+For example: 
+ average1= (player1+ player2+player3)/3  # average using  ‘/’ operator
+ average2= (player1+player2+player3)//3 # average using ‘//’ operator
+Explain your observations. Remove the parenthesis from the equations and print average1 and average2 again.  
+Give screenshots of the code, outputs and the git repository
+
+'''
