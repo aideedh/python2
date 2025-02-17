556421553deb8497e44172c7025ce6e0f5de871a
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-16 23:33:58 -0500

    create function start for loop

--- Assignment2/2. Use of for loop.py
+++ Assignment2/2. Use of for loop.py
@@ -1,2 +1,6 @@
 # Write a program that uses for loop to calculate the magnitude of a vector, where all dimensions (x,y,z)  
 # of the vector are integers entered by the user. Flowchart is required in this program.
+
+def calcMagnitude():
+
+    for loop in range (1,4):
