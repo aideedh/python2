69f77f0d21b0113122a3c80c96768d54dc857330
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 02:41:54 -0500

    finishing touches

--- Assignment2/3. Use of a while loop.py
+++ Assignment2/3. Use of a while loop.py
@@ -1,4 +1,4 @@
-# a)	Write a program that takes integer inputs from a user in a while loop in a variable called num
+# a) Write a program that takes integer inputs from a user in a while loop in a variable called num
 # and prints the value of num. The loop terminates if the user types a negative number. 
 # The program should also count the number of times the loop iterates.
 # Student should provide a flow chart for this problem
@@ -7,18 +7,24 @@ def huhu():
 
     loop = True
     count = 0
-    value = 0 
+    sum = 0 
 
     while loop:
-        
+        # user input in while loop
         num = int(input("Enter a number: "))
 
         if num >= 0:
             # implement logic for if number is greater than 0 
-            count+=1 
+            count+=1
+            sum+=num  # sum of all positive user inputs
         else:
             # implement logic if number is negative / less than 0 
             print("Sorry, only numbers >= 0 are Allowed!")
             print("Terminating ... .. .")
             break
 
+    # loop termination logic 
+
+    print("Total loop iterations:", count)
+
+    return 
