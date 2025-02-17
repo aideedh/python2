8da6e942ad79c8b82f919bb20b206a4c1efe8d37
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 04:59:10 -0500

    finished! - with ref and comments

--- Assignment2/5. Pattern Pyramid.py
+++ Assignment2/5. Pattern Pyramid.py
@@ -11,21 +11,24 @@
 #     test = x * times
 #     print (f"{x} x {times} = {test}" )
 
+def pyramid():
 
-ans = int(input("Number of rows: "))
+    ans = int(input("Number of rows: "))
 
-# set up ans+1 because it starts at 1 or it will be 1 less row
-for rows in range(1, ans+1):
+    # set up ans+1 because it starts at 1 or it will be 1 less row
+    for rows in range(1, ans+1):
 
-    # set up space (user input - current itteration of rows)
-    space = ans - rows
-    # so if user = 6   current itteration = 1  so 6 - 1 = 5  than  " "*5 spaces infront 
+        # set up space (user input - current itteration of rows)
+        space = ans - rows
+        # so if user = 6   current itteration = 1  so 6 - 1 = 5  than  " "*5 spaces infront 
 
-    if rows % 2 != 0:
-        print (" "*(space) + ("+"*((rows*2)-1)))
-    else:
-        print (" "*(space) + ("-"*((rows*2)-1)))
+        if rows % 2 != 0:
+            print (" "*(space) + ("+"*((rows*2)-1)))
+        else:
+            print (" "*(space) + ("-"*((rows*2)-1)))
 
+
+pyramid()
 
 # # refrence used  : https://www.youtube.com/watch?v=ynEHb4kE6F0
 # r = 9 
