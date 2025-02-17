c900ef4415f264320926ef1c59375b3500ad652c
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 02:32:17 -0500

    inital set up

---
+++ Assignment2/3. Use of a while loop.py
@@ -0,0 +1,6 @@
+# a)	Write a program that takes integer inputs from a user in a while loop in a variable called num
+# and prints the value of num. The loop terminates if the user types a negative number. 
+# The program should also count the number of times the loop iterates.
+# Student should provide a flow chart for this problem
+
+
