beb620db6aaf25c6080e62201368d44bd894198e
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 04:12:27 -0500

    first pattern

--- Assignment2/5. Pattern Pyramid.py
+++ Assignment2/5. Pattern Pyramid.py
@@ -1,5 +1,20 @@
 
-for number in range (1,4):
-    for times in range (1,4):
-        ans = number * times
-        print (f"{number} x {times} = {ans}" )
+# multiplication tables
+
+# for number in range (1,4):
+#     for times in range (1,4):
+#         ans = number * times
+#         print (f"{number} x {times} = {ans}" )
+
+# x = 1
+# for times in range(1,4):
+#     test = x * times
+#     print (f"{x} x {times} = {test}" )
+
+
+
+for rows in range (1,5):
+    if rows % 2 != 0:
+        print ("+"*rows)
+    else:
+        print ("-"*rows)
