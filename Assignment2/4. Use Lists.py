91888e425db678824923a7fa123b34883cc04553
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 02:53:30 -0500

    start logic

---
+++ Assignment2/4. Use Lists.py
@@ -0,0 +1,20 @@
+# Write a program to calculate standard deviation of a list of integers. 
+# This program should use a function called Avg() that calculates and returns average of the list passed 
+# as a parameter. 
+# The main program passes that average to another function called sumSqDiff()
+# that also accepts a list as an argument and returns the sum square of the difference of list values 
+# and the average. sumSqDiff() implements this formula ssd
+
+
+
+def Avg(list):
+
+    # Thinking:
+    # list passed as a paramenter
+    # so the sum of the list must be done than divided by count of list
+
+    for number in list:
+        # for loop to calc the sum of the list
+        
+
+
