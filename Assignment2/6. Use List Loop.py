7449f78024fa7d0d8feac16d7c448c3e5613d766
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 05:22:33 -0500

    add grade function

--- Assignment2/6. Use List Loop.py
+++ Assignment2/6. Use List Loop.py
@@ -5,4 +5,38 @@
 # score between 60 and 70 is grade C, score between 50 and 60 is grade D and score less than 50 is grade D. 
 # Use any loop of you choice. Student must provide a flowchart for this program
 
-scores = []
+
+def Grade():
+    
+    average = Avg(scores)
+
+    if average > 80:
+        print("Grade: A")
+    elif average > 70 and average < 80 :
+        print("Grade: B")
+    elif average > 60 and average < 70 :
+        print("Grade: C")
+    elif average > 50 and average < 60 :
+        print("Grade: D")
+    else:
+        print("Grade: D")
+
+
+def Avg(list):
+
+    # calculate sum
+    total = 0 
+    for number in list:
+        total+=number
+    # get amount of elements in list
+    elements = len(list)
+
+    score = (total/elements)
+    
+    return score
+
+
+
+scores = [96,90,82,92,76,64,75,81,90,72]
+
+print(Grade())
