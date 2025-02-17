6c9aa39923e72a73ad6da8f2bcb1ce1ebbc5a602
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-16 23:31:14 -0500

    set up q2

--- Assignment2/2. Use of for loop.py
+++ Assignment2/2. Use of for loop.py
@@ -1,1 +1,2 @@
-# test#
+# Write a program that uses for loop to calculate the magnitude of a vector, where all dimensions (x,y,z)  
+# of the vector are integers entered by the user. Flowchart is required in this program.
