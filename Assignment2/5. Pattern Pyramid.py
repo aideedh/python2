446b9a0c0072e59035a22110737195f659de91ee
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 04:53:55 -0500

    finished!

--- Assignment2/5. Pattern Pyramid.py
+++ Assignment2/5. Pattern Pyramid.py
@@ -14,10 +14,20 @@
 
 ans = int(input("Number of rows: "))
 
+# set up ans+1 because it starts at 1 or it will be 1 less row
 for rows in range(1, ans+1):
 
+    # set up space (user input - current itteration of rows)
     space = ans - rows
+    # so if user = 6   current itteration = 1  so 6 - 1 = 5  than  " "*5 spaces infront 
+
     if rows % 2 != 0:
         print (" "*(space) + ("+"*((rows*2)-1)))
     else:
         print (" "*(space) + ("-"*((rows*2)-1)))
+
+
+# # refrence used  : https://www.youtube.com/watch?v=ynEHb4kE6F0
+# r = 9 
+# for i in range (r):
+#     print(" " *(r-i), "*"*(i*2+1))
