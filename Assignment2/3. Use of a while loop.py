088300b4c756fb4cdfe7e8df15b15c258f7a53c2
aideed <83303507+aideedh@users.noreply.github.com>
2025-02-17 02:38:25 -0500

    if else logic after user input

--- Assignment2/3. Use of a while loop.py
+++ Assignment2/3. Use of a while loop.py
@@ -3,4 +3,22 @@
 # The program should also count the number of times the loop iterates.
 # Student should provide a flow chart for this problem
 
+def huhu():
+
+    loop = True
+    count = 0
+    value = 0 
+
+    while loop:
+        
+        num = int(input("Enter a number: "))
+
+        if num >= 0:
+            # implement logic for if number is greater than 0 
+            count+=1 
+        else:
+            # implement logic if number is negative / less than 0 
+            print("Sorry, only numbers >= 0 are Allowed!")
+            print("Terminating ... .. .")
+            break
 
