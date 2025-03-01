06dcd8151be13f3b7ae578f11c252a658d01a08c
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-01 12:14:44 -0500

    set up for cube

---
+++ Assignment3/Shapev1.py
@@ -0,0 +1,14 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("aqua")
+
+myturtle = turtle.Turtle()
+myturtle.shape = ("Turtle")
+myturtle.color = ("Blue")
+
+myturtle.forward(100)
+myturtle.left(90)
+
+
+screen.exitonclick() 
