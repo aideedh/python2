28bf22c45bd122c77e76a07c1ef9dc685e49bdd2
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-09 18:04:28 -0400

    withdraw method

--- Assignment3/Banking.py
+++ Assignment3/Banking.py
@@ -1,6 +1,23 @@
 
 class Account:
-    pass
+    
+    def __init__(self, accountType, minBalance, currentBalance):
+        self.accountType = accountType
+        self.minBalance = minBalance 
+        self.currentBalance = currentBalance
+
+    def withdraw(self, amount):
+        
+        if self.accountType == "chequing":
+            overdraft = self.currentBalance - amount
+            if overdraft < -1000:
+                print(f"Sorry you have exceeded overdraft limit by : {overdraft}")
+            else:
+                self.currentBalance -= amount
+                print(self.currentBalance)
+
+
+
 
 class Chequing(Account):
     pass
