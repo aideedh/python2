58f1be657f4cbfa9dffdf0620db14036721b8a7d
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-09 17:49:01 -0400

    finished! with all comments and program

--- Assignment3/GameRemake.py
+++ Assignment3/GameRemake.py
@@ -14,7 +14,7 @@ class Player:
         return self.score
     
     def roll(self):
-        diceRoll = random.randint(1,6)
+        diceRoll = random.randint(1,3)
         return diceRoll
 
 class Game:
@@ -26,7 +26,7 @@ class Game:
         self.players.append(players)
 
     def guess(self):
-        guess = random.randint(1,6)
+        guess = random.randint(1,3)
         return guess
 
     def match(self):
@@ -42,12 +42,13 @@ class Game:
             if roll == roundGuess:
                 player.updateScore(1)
                 print(f"{player.name} has matched, +1 point!")
+                # after 1 player matches it moves to next itteration of loop instead of else statment
+                continue
             
             else:
                 print("No one won!")
+                # this break is to stop it from repeating messege
                 break
-
-
 
     def getPlayerScore(self):
         # so this is to get the score of each player in list
@@ -56,20 +57,26 @@ class Game:
 
 class Application:
 
+    # creating instance of an object
     gameStart = Game()
+
+    # creating player objects
     playerOne = Player("playerOne")
     playerTwo = Player("playerTwo")
     playerThree = Player("playerThree")
+
+    # passing player objects to game object
     gameStart.addPlayers(playerOne)
     gameStart.addPlayers(playerTwo) 
     gameStart.addPlayers(playerThree)
 
-    for loop in range(5):
+    # running the game using match function 5 times
+    for loop in range(1,6):
         print(f"Staring round {loop}:")
         gameStart.match()
-        
+    
+    # finished
     print("------------------")
     print("Final Scores")
     print("------------------")
-
     gameStart.getPlayerScore()
