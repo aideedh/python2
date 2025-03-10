5faae51f4c67914e2e312b67bee1da0be440f659
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-09 22:10:17 -0400

    savings done?

--- Assignment3/Banking.py
+++ Assignment3/Banking.py
@@ -21,13 +21,27 @@ class Account:
             overdraftProtection = self.currentBalance - amount
             if overdraftProtection < overdraft:
                 print(f"Sorry you have exceeded overdraft limit by : {overdraft}")
+                overdraft +=200
+                print("overdraft limit increased by 200")
+
             else:
                 self.currentBalance -= amount
                 print(f"Withdrawal successful!")
                 print(f"New balance: {self.currentBalance}")
 
         elif self.accountType == "savings":
-            
+
+            overdraft = -1200
+
+            overdraftProtection = self.currentBalance - amount
+            if overdraftProtection < overdraft:
+                print(f"Sorry you have exceeded overdraft limit by : {overdraft}")
+                overdraft +=200
+                print("overdraft limit increased by 200")
+            else:
+                self.currentBalance -= amount
+                print(f"Withdrawal successful!")
+                print(f"New balance: {self.currentBalance}")
 
 
 
