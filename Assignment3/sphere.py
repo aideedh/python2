e368ec9319e05204f6e76f524a448646ec675393
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-11 18:10:03 -0400

    finished! - old one is best one

--- Assignment3/sphere.py
+++ Assignment3/sphere.py
@@ -1,3 +1,36 @@
+# import turtle
+
+# # set screen and back ground color
+# screen = turtle.Screen()
+# screen.bgcolor("aqua")
+
+# # fixed the spelling error 
+# myturtle = turtle.Turtle()
+# myturtle.shape("turtle")
+# myturtle.color("blue")
+
+# num = 60
+
+# # for loop to repeat code
+# for loop in range (num, 0, -2):
+
+#     # reposition the turtle
+#     myturtle.penup()
+#     myturtle.goto(0, -num)
+#     myturtle.pendown()
+
+#     # this will decrease the number of loops
+#     # the lower it is the more space between circles
+#     num -= 2
+
+#     # function found trough googling
+#     myturtle.circle(num)
+
+# myturtle.penup()
+# myturtle.forward(150)
+# screen.exitonclick()
+
+
 import turtle
 
 # set screen and back ground color
@@ -14,11 +47,6 @@ num = 60
 # for loop to repeat code
 for loop in range (num, 0, -2):
 
-    # reposition the turtle
-    myturtle.penup()
-    myturtle.goto(0, -num)
-    myturtle.pendown()
-
     # this will decrease the number of loops
     # the lower it is the more space between circles
     num -= 2
@@ -26,5 +54,6 @@ for loop in range (num, 0, -2):
     # function found trough googling
     myturtle.circle(num)
 
-
+myturtle.penup()
+myturtle.forward(150)
 screen.exitonclick()
