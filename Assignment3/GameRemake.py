888cfd46ce8efc0df1373dc0f0553383cdaa01cd
aideed <83303507+aideedh@users.noreply.github.com>
2025-03-12 04:18:38 -0400

    fix colors

--- Assignment3/GameRemake.py
+++ Assignment3/GameRemake.py
@@ -1,94 +1,94 @@
 
-'''
+# '''
 
-Object passed to another object using method:
-------------------------------------------------
-This way is using addplayers(self, player) method to pass object to object
+# Object passed to another object using method:
+# ------------------------------------------------
+# This way is using addplayers(self, player) method to pass object to object
 
-'''
+# '''
 
 
-import random
+# import random
 
-class Player:
+# class Player:
 
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
+#     def __init__(self, name):
+#         self.name = name
+#         self.score = 0
 
-    def updateScore(self, addScore):
-        self.score += addScore
+#     def updateScore(self, addScore):
+#         self.score += addScore
 
-    def playerScore(self):
-        return self.score
+#     def playerScore(self):
+#         return self.score
     
-    def roll(self):
-        diceRoll = random.randint(1,3)
-        return diceRoll
+#     def roll(self):
+#         diceRoll = random.randint(1,3)
+#         return diceRoll
 
-class Game:
+# class Game:
     
-    def __init__(self):
-        self.players = []
+#     def __init__(self):
+#         self.players = []
 
-    def addPlayers(self, players):
-        self.players.append(players)
+#     def addPlayers(self, players):
+#         self.players.append(players)
 
-    def guess(self):
-        guess = random.randint(1,3)
-        return guess
+#     def guess(self):
+#         guess = random.randint(1,3)
+#         return guess
 
-    def match(self):
+#     def match(self):
         
-        # itterate over every player
+#         # itterate over every player
 
-        for player in self.players:
+#         for player in self.players:
 
-            roll = player.roll()
-            roundGuess = self.guess()
+#             roll = player.roll()
+#             roundGuess = self.guess()
 
-            # than check if players roll matched guess
-            if roll == roundGuess:
-                player.updateScore(1)
-                print(f"{player.name} has matched, +1 point!")
-                # after 1 player matches it moves to next itteration of loop instead of else statment
-                continue
+#             # than check if players roll matched guess
+#             if roll == roundGuess:
+#                 player.updateScore(1)
+#                 print(f"{player.name} has matched, +1 point!")
+#                 # after 1 player matches it moves to next itteration of loop instead of else statment
+#                 continue
             
-            else:
-                print("No one won!")
-                # this break is to stop it from repeating messege
-                break
+#             else:
+#                 print("No one won!")
+#                 # this break is to stop it from repeating messege
+#                 break
 
-    def getPlayerScore(self):
-        # so this is to get the score of each player in list
-        for players in self.players:
-            print (f"{players.name} - Score: {players.playerScore()}")
+#     def getPlayerScore(self):
+#         # so this is to get the score of each player in list
+#         for players in self.players:
+#             print (f"{players.name} - Score: {players.playerScore()}")
 
-class Application:
+# class Application:
 
-    # creating instance of an object
-    gameStart = Game()
+#     # creating instance of an object
+#     gameStart = Game()
 
-    # creating player objects
-    playerOne = Player("playerOne")
-    playerTwo = Player("playerTwo")
-    playerThree = Player("playerThree")
+#     # creating player objects
+#     playerOne = Player("playerOne")
+#     playerTwo = Player("playerTwo")
+#     playerThree = Player("playerThree")
 
-    # passing player objects to game object
-    gameStart.addPlayers(playerOne)
-    gameStart.addPlayers(playerTwo) 
-    gameStart.addPlayers(playerThree)
+#     # passing player objects to game object
+#     gameStart.addPlayers(playerOne)
+#     gameStart.addPlayers(playerTwo) 
+#     gameStart.addPlayers(playerThree)
 
-    # running the game using match function 5 times
-    for loop in range(1,6):
-        print(f"Staring round {loop}:")
-        gameStart.match()
+#     # running the game using match function 5 times
+#     for loop in range(1,6):
+#         print(f"Staring round {loop}:")
+#         gameStart.match()
     
-    # finished
-    print("------------------")
-    print("Final Scores")
-    print("------------------")
-    gameStart.getPlayerScore()
+#     # finished
+#     print("------------------")
+#     print("Final Scores")
+#     print("------------------")
+#     gameStart.getPlayerScore()
 
 
 
--- Assignment3/Shapev1.py
+++ Assignment3/Shapev1.py
@@ -3,11 +3,11 @@ import turtle
 # this program will make a cube
 
 screen = turtle.Screen()
-screen.bgcolor("aqua")
+screen.bgcolor("white")
 
 myturtle = turtle.Turtle()
-myturtle.shape = ("Turtle")
-myturtle.color = ("Blue")
+myturtle.shape("turtle")
+myturtle.color("blue")
 
 # Draw the front square
 for loop in range(1,5):
@@ -15,15 +15,18 @@ for loop in range(1,5):
     myturtle.right(90)
 
 # Draw the back Square
+myturtle.color("Red")
 myturtle.left(45)
 myturtle.forward(50)
 myturtle.right(45)
 
+myturtle.color("yellow")
 for _ in range(4):
     myturtle.forward(100)
     myturtle.right(90)
 
 # draw Top squre
+myturtle.color("green")
 myturtle.penup
 myturtle.forward(100)
 myturtle.right(90)
@@ -32,12 +35,14 @@ myturtle.forward(50)
 
 
 # Reposition to the bottom left corner  of first sq
+myturtle.color("black")
 myturtle.left(45)
 myturtle.forward(100)
 myturtle.right(90)
 myturtle.forward(100)
 
 # draw sq behind front sq
+myturtle.color("pink")
 myturtle.right(135)
 myturtle.forward(50)
 myturtle.right(45)
@@ -45,6 +50,9 @@ myturtle.forward(100)
 myturtle.right(135)
 myturtle.forward(50)
 
+# move turtle away from cube
 
+myturtle.penup()
+myturtle.forward(50) 
 
 screen.exitonclick()
